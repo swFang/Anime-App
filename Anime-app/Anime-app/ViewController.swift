c1566f80eae01e58c964fd268a2914e2873b30ca
Daniel Fang <danielfang@hotmail.com>
2020-06-07 18:12:31 -0700

    bug fixing
    Previously pushed up some faulty code...

--- Anime-app/Anime-app/ViewController.swift
+++ Anime-app/Anime-app/ViewController.swift
@@ -9,35 +9,44 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, WKNavigationDelegate {
+class ViewController: UIViewController, WKNavigationDelegate, UIScrollViewDelegate {
     var webView: WKWebView!
+    var scrollView: UIScrollView!
     
     override func loadView() {
         webView = WKWebView()
+        scrollView = UIScrollView()
+        
         webView.navigationDelegate = self
+        self.scrollView.delegate = self
         view = webView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         let url = URL(string: "https://4anime.to/")!
         webView.load(URLRequest(url: url))
-        webView.scrollView.showsHorizontalScrollIndicator = false
         webView.allowsBackForwardNavigationGestures = true
         
-        print("finish to load")
-        let elType = "right_sidebar"
-        let removeElementIdScript = "var element = document.getElementByTagName('\(elType)'); element.parentElement.removeChild(element);"
+        let elType = "table"
+        let removeElementIdScript = "var element = document.getElementByTagName('\(elType)'); element[0].parentElement.removeChild(element);"
         webView.evaluateJavaScript(removeElementIdScript) { (response, error) in
             debugPrint("Am here")
+        }
+        
+        //currently doesnt work this section, cant disable horizontal scroll..
+        let body = "body"
+        let noHorizontalScroll = "el = document.getElementsByTagName('\(body)'); el[0].style.overflow = 'hidden'"
+        webView.evaluateJavaScript(noHorizontalScroll) { (response, error) in
+            debugPrint("disablescroll")
         }
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-       if (scrollView.contentOffset.x > 0){
-           scrollView.contentOffset = CGPoint(x: 0, y: scrollView.contentOffset.y)
-       }
+        print("scrollviewdidscroll")
+        if scrollView.contentOffset.x != 0 {
+               scrollView.contentOffset.x = 0
+        }
     }
 }
 
